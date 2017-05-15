3bc66982d431b344ad6303bbc69265984451ecde
tamas <tomi20v@github.com>
2017-05-15 05:54:00 +0200

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+server
+======
+
+A Symfony project created on May 12, 2017, 7:59 am.
---
+++ src/AppBundle/Entity/Archive.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Archive
+ *
+ * @ORM\Table(name="archive")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ArchiveRepository")
+ */
+class Archive
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fname", type="string", length=255)
+     */
+    private $fname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="blob")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fname
+     *
+     * @param string $fname
+     *
+     * @return Archive
+     */
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+
+        return $this;
+    }
+
+    /**
+     * Get fname
+     *
+     * @return string
+     */
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Archive
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
+
